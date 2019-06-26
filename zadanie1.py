9b8219505abc90e7d0bdd5a1a427429c24f422a3
i.andrejewicz@gmail.com <i.anrejewicz@gmail.com>
2019-06-26 16:57:21 +0200

    zadanie1, zadanie2 corrected

--- zadanie1.py
+++ zadanie1.py
@@ -1,6 +1,4 @@
 def father_twice_old(father_age, son_age):
     if -(father_age - 2 * son_age) < 0:
         return f'Było to {abs(-(father_age - 2 * son_age))} lat temu'
-    else:
-        return f'Będzie to za {-(father_age - 2 * son_age)} lat'
-
+    return f'Będzie to za {-(father_age - 2 * son_age)} lat'
--- zadanie2.py
+++ zadanie2.py
@@ -1,9 +1,7 @@
 def check_duplicates(sentence):
-    lista = sentence.split(" ")
     x = []
-    for i in lista:
-        if i not in x:
-            x.append(i)
+    [x.append(i) for i in sentence.split(" ") if i not in x ]
     return ' '.join(x)
 
 assert check_duplicates("alpha beta beta gamma gamma gamma delta gamma delta beta alpha gamma") == 'alpha beta gamma delta'
+# print(check_duplicates("alpha beta beta gamma gamma gamma delta gamma delta beta alpha gamma"))
